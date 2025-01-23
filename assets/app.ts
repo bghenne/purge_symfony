7add2252054f0f7036562089e008aac9b2249b62
Benjamin Ghenne <benjamin.ghenne@gfptech.fr>
2025-01-23 14:45:43 +0100

    Got rid of symfony sucking packages

---
+++ assets/app.ts
@@ -0,0 +1,29 @@
+/*
+ * Welcome to your app's main JavaScript file!
+ *
+ * This file will be included onto the page via the importmap() Twig function,
+ * which should already be in your base.html.twig.
+ */
+import './styles/app.css';
+
+import { createApp } from 'vue'
+
+// Plugins
+//import i18n from './plugins/i18n'
+//import translations from './i18n.json'
+
+// Directives
+//import { autofocus } from './directives/autofocus'
+
+// Internal components
+import App from './App.vue'
+//import router from './router'
+
+// CSS
+//import './assets/css/main.css'
+
+createApp(App)
+    //.directive('autofocus', autofocus)
+    //.use(router)
+    //.use(i18n, translations)
+    .mount('#app');
--- webpack.config.js
+++ webpack.config.js
@@ -20,12 +20,10 @@ Encore
      * Each entry will result in one JavaScript file (e.g. app.js)
      * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
      */
-    .addEntry('app', './assets/app.js')
+    .addEntry('app', './assets/app.ts')
 
     // When enabled, Webpack "splits" your files into smaller pieces for greater optimization.
     .splitEntryChunks()
-
-    .enableVueLoader()
 
     // will require an extra script tag for runtime.js
     // but, you probably want this, unless you're building a single-page app
@@ -59,7 +57,8 @@ Encore
     //.enableSassLoader()
 
     // uncomment if you use TypeScript
-    //.enableTypeScriptLoader()
+    .enableVueLoader(() => {}, { runtimeCompilerBuild: false })
+    .enableTypeScriptLoader()
 
     // uncomment if you use React
     //.enableReactPreset()
