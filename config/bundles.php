29d78a540ac411be2abbefa81f0693b400990297
Benjamin Ghenne <benjamin.ghenne@gfptech.fr>
2025-02-05 17:25:37 +0100

    Added vite support to symfony application

--- config/bundles.php
+++ config/bundles.php
@@ -10,5 +10,5 @@ return [
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Drenso\OidcBundle\DrensoOidcBundle::class => ['all' => true],
-    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
+    Pentatrion\ViteBundle\PentatrionViteBundle::class => ['all' => true]
 ];
---
+++ vite.config.js
@@ -0,0 +1,21 @@
+import { defineConfig } from "vite";
+import symfonyPlugin from "vite-plugin-symfony";
+import vuePlugin from "@vitejs/plugin-vue";
+
+/* if you're using React */
+// import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+    plugins: [
+        /* react(), // if you're using React */
+        symfonyPlugin(),
+        vuePlugin()
+    ],
+    build: {
+        rollupOptions: {
+            input: {
+                app: "./assets/app.ts"
+            },
+        }
+    },
+});
