67f5a295abfe1d0fb2eb933f50553ab5df862460
Charles Nannan <charles.nannan@gfptech.fr>
2025-02-25 15:22:09 +0100

    Improve a bit and clean up the navigation and sorting

--- assets/vue/pages/EligibleObject.vue
+++ assets/vue/pages/EligibleObject.vue
@@ -1,15 +1,46 @@
 <template>
-
-  <div class="card flex justify-center mb-4">
-    <Toast/>
-    <Form v-slot="$eligibleObjectForm" :resolver="resolver" @submit="onFormSubmit" ref="eligible-object-form">
+  <div class="card mb-4 flex justify-center">
+    <Toast />
+    <Form
+      v-slot="$eligibleObjectForm"
+      :resolver="resolver"
+      @submit="onFormSubmit"
+      ref="eligible-object-form"
+    >
       <div class="flex gap-5">
-        <Select v-model="environment" name="environment" :options="environments" optionLabel="name" placeholder="Choisissez un environnement" fluid checkmark/>
-        <Message v-if="$eligibleObjectForm.environment?.invalid" severity="error" size="small" variant="simple">
+        <Select
+          v-model="environment"
+          name="environment"
+          :options="environments"
+          optionLabel="name"
+          placeholder="Choisissez un environnement"
+          fluid
+          checkmark
+        />
+        <Message
+          v-if="$eligibleObjectForm.environment?.invalid"
+          severity="error"
+          size="small"
+          variant="simple"
+        >
           {{ $eligibleObjectForm.environment.error.message }}
         </Message>
-        <Select v-model="theme" name="theme" :options="themes" optionLabel="name" :loading="fetchingThemes" placeholder="Choisissez un thème" fluid checkmark/>
-        <Message v-if="$eligibleObjectForm.theme?.invalid" severity="error" size="small" variant="simple">
+        <Select
+          v-model="theme"
+          name="theme"
+          :options="themes"
+          optionLabel="name"
+          :loading="fetchingThemes"
+          placeholder="Choisissez un thème"
+          fluid
+          checkmark
+        />
+        <Message
+          v-if="$eligibleObjectForm.theme?.invalid"
+          severity="error"
+          size="small"
+          variant="simple"
+        >
           {{ $eligibleObjectForm.theme.error.message }}
         </Message>
 
@@ -19,27 +50,52 @@
                  (that would be invalid). It leverages the Web platform behaviour for forms submission,
                  e.g. allowing submission from any input by pressing the "enter" key. -->
             <form
-                class="grid grid-cols-2 gap-3"
-                @submit.prevent="eligibleObjectForm.$el.requestSubmit()"
+              class="grid grid-cols-2 gap-3"
+              @submit.prevent="eligibleObjectForm.$el.requestSubmit()"
             >
               <label class="flex flex-col">
                 <span class="font-bold">Du</span>
-                <DatePicker name="dateFrom" v-model="dateFrom" dateFormat="dd/mm/yy" placeholder="jj/mm/aaaa"/>
-                <Message v-if="$eligibleObjectForm.dateFrom?.invalid" severity="error" size="small" variant="simple">
+                <DatePicker
+                  name="dateFrom"
+                  v-model="dateFrom"
+                  dateFormat="dd/mm/yy"
+                  placeholder="jj/mm/aaaa"
+                />
+                <Message
+                  v-if="$eligibleObjectForm.dateFrom?.invalid"
+                  severity="error"
+                  size="small"
+                  variant="simple"
+                >
                   {{ $eligibleObjectForm.dateFrom.error.message }}
                 </Message>
               </label>
               <label class="flex flex-col">
                 <span class="font-bold">Au</span>
-                <DatePicker name="dateTo" v-model="dateTo" dateFormat="dd/mm/yy" placeholder="jj/mm/aaaa"/>
-                <Message v-if="$eligibleObjectForm.dateTo?.invalid" severity="error" size="small" variant="simple">
+                <DatePicker
+                  name="dateTo"
+                  v-model="dateTo"
+                  dateFormat="dd/mm/yy"
+                  placeholder="jj/mm/aaaa"
+                />
+                <Message
+                  v-if="$eligibleObjectForm.dateTo?.invalid"
+                  severity="error"
+                  size="small"
+                  variant="simple"
+                >
                   {{ $eligibleObjectForm.dateTo.error.message }}
                 </Message>
               </label>
               <label class="flex flex-col">
                 <span class="font-bold">ID de famille</span>
-                <InputText name="familyId" v-model="familyId" type="text"/>
-                <Message v-if="$eligibleObjectForm.familyId?.invalid" severity="error" size="small" variant="simple">
+                <InputText name="familyId" v-model="familyId" type="text" />
+                <Message
+                  v-if="$eligibleObjectForm.familyId?.invalid"
+                  severity="error"
+                  size="small"
+                  variant="simple"
+                >
                   {{ $eligibleObjectForm.familyId.error.message }}
                 </Message>
               </label>
@@ -50,58 +106,123 @@
               <!--                      checkmark/>-->
               <!--            </div>-->
 
-              <Button type="reset" label="Effacer" severity="secondary" class="row-start-3 shrink-0 mt-4" :disabled="null === dateFrom && null === dateTo && null === familyId"
-                      @click="resetAdvancedSearchValues" />
-              <Button type="submit" label="Valider" severity="primary" class="row-start-3 shrink-0 mt-4" :disabled="searchInProgress || null === dateFrom && dateTo || dateFrom && null === dateTo" :loading="searchInProgress" />
+              <Button
+                type="reset"
+                label="Effacer"
+                severity="secondary"
+                class="row-start-3 mt-4 shrink-0"
+                :disabled="
+                  null === dateFrom && null === dateTo && null === familyId
+                "
+                @click="resetAdvancedSearchValues"
+              />
+              <Button
+                type="submit"
+                label="Valider"
+                severity="primary"
+                class="row-start-3 mt-4 shrink-0"
+                :disabled="
+                  searchInProgress ||
+                  (null === dateFrom && dateTo) ||
+                  (dateFrom && null === dateTo)
+                "
+                :loading="searchInProgress"
+              />
             </form>
           </AdvancedSearch>
         </Teleport>
 
-        <Button type="reset" label="Effacer" severity="secondary" class="shrink-0" :disabled="null === theme && null === environment"  @click="resetBasicSearchValues" />
-        <Button type="submit" label="Rechercher" severity="primary" class="shrink-0" :disabled="searchInProgress || null === theme || null === environment" :loading="searchInProgress" />
+        <Button
+          type="reset"
+          label="Effacer"
+          severity="secondary"
+          class="shrink-0"
+          :disabled="null === theme && null === environment"
+          @click="resetBasicSearchValues"
+        />
+        <Button
+          type="submit"
+          label="Rechercher"
+          severity="primary"
+          class="shrink-0"
+          :disabled="searchInProgress || null === theme || null === environment"
+          :loading="searchInProgress"
+        />
       </div>
     </Form>
   </div>
 
   <DataTable
-      ref="eligibleObjectsTable"
-      v-model:expandedRows="eligibleObjects.details"
-      dataKey="key"
-      :value="eligibleObjects"
-      :totalRecords="totalRecords" @page="onPage($event)" @sort="onSort($event)"
-      removableSort
-      paginator
-      :lazy="true"
-      :rows="10"
-      :loading="searchInProgress"
-      paginatorTemplate="RowsPerPageDropdown FirstPageLink PrevPageLink CurrentPageReport NextPageLink LastPageLink"
-      currentPageReportTemplate="{first} à {last} sur {totalRecords}"
+    ref="eligibleObjectsTable"
+    v-model:expandedRows="eligibleObjects.details"
+    dataKey="key"
+    :value="eligibleObjects"
+    :totalRecords="totalRecords"
+    @page="onPage($event)"
+    @sort="onSort($event)"
+    removableSort
+    paginator
+    :lazy="true"
+    :rows="10"
+    :loading="searchInProgress"
+    paginatorTemplate="RowsPerPageDropdown FirstPageLink PrevPageLink CurrentPageReport NextPageLink LastPageLink"
+    currentPageReportTemplate="{first} à {last} sur {totalRecords}"
   >
-    <Column expander style="width: 5rem"/>
+    <Column expander style="width: 5rem" />
     <Column field="familyId" header="Identifiant de famille" sortable></Column>
-    <Column field="contributionPaymentDate" header="Date de dernier paiment de la cotisation" sortable></Column>
-    <Column field="contributionCallPeriod" header="Période des cotisations en mois" sortable></Column>
-    <Column field="contributionCallYear" header="Période des cotisations en année" sortable></Column>
-    <Column field="conservationTime" header="Délai de conservation appliqué"></Column>
+    <Column
+      field="contributionPaymentDate"
+      header="Date de dernier paiment de la cotisation"
+      sortable
+    ></Column>
+    <Column
+      field="contributionCallPeriod"
+      header="Période des cotisations en mois"
+      sortable
+    ></Column>
+    <Column
+      field="contributionCallYear"
+      header="Période des cotisations en année"
+      sortable
+    ></Column>
+    <Column
+      field="conservationTime"
+      header="Délai de conservation appliqué"
+    ></Column>
     <Column field="clientName" header="Nom du client"></Column>
     <template #expansion="slotProps">
       <div class="p-4">
         <p>Nom : {{ slotProps.data.details.beneficiaryName }}</p>
         <p>Prénom : {{ slotProps.data.details.beneficiaryFirstname }}</p>
-        <p>Numéro de sécurité sociale : {{ slotProps.data.details.socialSecurityNumber }}</p>
-        <p>Date de naissance du bénéficiaire : {{ slotProps.data.details.beneficiaryBirthdate }}</p>
+        <p>
+          Numéro de sécurité sociale :
+          {{ slotProps.data.details.socialSecurityNumber }}
+        </p>
+        <p>
+          Date de naissance du bénéficiaire :
+          {{ slotProps.data.details.beneficiaryBirthdate }}
+        </p>
       </div>
     </template>
   </DataTable>
-
 </template>
 
 <script setup lang="ts">
 
 import {EligibleObject, EligibleObjects} from "../types/eligible-object";
-import {Button, Column, DataTable, DataTablePageEvent, InputText, Message, Select, Toast} from "primevue";
+import {
+  Button,
+  Column,
+  DataTable,
+  DataTablePageEvent, DataTableSortEvent,
+  DataTableState,
+  InputText,
+  Message,
+  Select,
+  Toast
+} from "primevue";
 import {Form} from "@primevue/forms";
-import {ref, useTemplateRef, watch} from 'vue';
+import {ref, useTemplateRef} from 'vue';
 import {useToast} from "primevue/usetoast";
 import {ObjectType} from "../enums/object-type";
 import {doRequest} from "../utilities/request";
@@ -131,11 +252,13 @@ const searchInProgress = ref(false);
 const totalRecords = ref(0);
 const advancedSearchDisplayed = ref(false);
 
-// Store pagination and sort stuffs
-const eligibleObjectsTable = ref(null);
-const sortField = ref(null);
-const sortOrder = ref(null);
-const paginationPage = ref(null);
+// To use the pagination and sorting features along with lazy loading,
+// we need to read and update the DataTable internal state (a limitation of PrimeVue).
+const eligibleObjectsTable = ref({} as DataTableState);
+
+// As of PrimeVue 4.2.5, the DataTable component state does not include
+// the current page, which must be tracked here.
+const paginationPage = ref(0);
 
 getEnvironments();
 fetchThemes(ObjectType.ELIGIBLE);
@@ -163,7 +286,6 @@ const resolver = ({values}) => {
 }
 
 const onFormSubmit = ({originalEvent, valid, values}) => {
-
   // if form is posted from main search perspective, we reset the advanced one
   // SubmitEvent.submitter returns null if a form is submitted programmatically.
   if (null !== originalEvent.submitter) {
@@ -175,6 +297,7 @@ const onFormSubmit = ({originalEvent, valid, values}) => {
   }
 
   // every time form is submitted, we reset pagination and sort
+  // TODO: maybe call this only if the request is successful, and sets the data to null if it is not
   resetPaginationAndSort();
 
   if (valid) {
@@ -202,12 +325,10 @@ const onFormSubmit = ({originalEvent, valid, values}) => {
     }
 
     findEligibleObjects(formData);
-
   }
 };
 
 const findEligibleObjects = (formData : FormData) => {
-
   searchInProgress.value = true;
 
   doRequest('/api/eligible-object', Methods.POST, formData)
@@ -223,13 +344,9 @@ const findEligibleObjects = (formData : FormData) => {
       })
       .catch(error => toast.add({severity: 'error', summary: 'Une erreur s\'est produite :' + error, life: 5000}))
       .finally(() => searchInProgress.value = false)
-
 }
 
 const onPage = (event: DataTablePageEvent) => {
-
-  console.log(event)
-
   const formData = new FormData;
   formData.append('environment', environment.value.name);
   formData.append('theme', theme.value.code);
@@ -246,26 +363,26 @@ const onPage = (event: DataTablePageEvent) => {
     formData.append('familyId', familyId.value);
   }
 
-  const page = 0 === event.page ? event.page : event.page++;
-  formData.append('page', page);
+  formData.append('page', String(event.page));
 
-  // store pagination
-  paginationPage.value = page;
+  // The current page is not included in the DataTable component state.
+  // So in order to access it and restore it in the onSort callback
+  // (which otherwise resets it), it must first be set here.
+  paginationPage.value = event.page;
 
-  // look for sort stuffs
-  if (null !== sortField.value && null !== sortOrder.value) {
-    formData.append('sortField', sortField.value);
-    formData.append('sortOrder', sortOrder.value);
+  // Checks if sorting criteria must be passed to the webservice.
+  // Note that the sorting criteria are not available in DataTablePageEvent.
+  // The workaround is to retrieve them from the component state.
+  if (undefined !== eligibleObjectsTable.value.d_sortField) {
+    formData.append('sortField', eligibleObjectsTable.value.d_sortField as string);
+    formData.append('sortOrder', String(eligibleObjectsTable.value.d_sortOrder));
   }
 
+  console.log(Object.fromEntries(formData));
   findEligibleObjects(formData);
-
 }
 
-const onSort = (event) => {
-
-  console.log(event)
-
+const onSort = (event: DataTableSortEvent) => {
   const formData = new FormData;
   formData.append('environment', environment.value.name);
   formData.append('theme', theme.value.code);
@@ -282,25 +399,27 @@ const onSort = (event) => {
     formData.append('familyId', familyId.value);
   }
 
-  // sortField and sortOrder can be null when sort is reset
+  // sortField and sortOrder evaluate to null when a sorted column becomes unsorted again.
   // @see https://primevue.org/datatable/#api.datatable.props.removableSort
   // therefore we must remove them from search
   if (null !== event.sortField) {
-    formData.append('sortField', event.sortField);
-    sortField.value = event.sortField;
+    formData.append('sortField', event.sortField as string);
   }
 
   if (null !== event.sortOrder) {
-    formData.append('sortOrder', event.sortOrder);
-    sortOrder.value = event.sortOrder;
+    formData.append('sortOrder', String(event.sortOrder));
   }
 
-  if (null !== paginationPage.value) {
-    formData.append('page', paginationPage.value);
-  }
+  formData.append('page', String(paginationPage.value));
 
+  // This prevents the current page from being reset.
+  eligibleObjectsTable.value.d_first = paginationPage.value * eligibleObjectsTable.value.d_rows;
+
+  console.log('event.sortField');
+  console.log(event.sortField);
+  console.log('event.sortOrder');
+  console.log(event.sortOrder);
   findEligibleObjects(formData);
-
 }
 
 function resetBasicSearchValues(event: MouseEvent) {
@@ -316,21 +435,21 @@ function resetAdvancedSearchValues(event: MouseEvent) {
   familyId.value = null;
 }
 
-function resetPaginationAndSort() {
-
-  // as the data are being reloaded by form submission, we need to tell datatable to "reset" itself
+/**
+ * Resets the current page and the DataTable component state.
+ *
+ * It is not reset by default when the bound value changes after a form submission.
+ */
+function resetPaginationAndSort(): void {
+  // First page displayed (1st row means 1st page).
   eligibleObjectsTable.value.d_first = 0;
-  eligibleObjectsTable.value.d_page = 1;
-  eligibleObjectsTable.value.page = 1;
-  eligibleObjectsTable.value.d_sortField = null;
-  eligibleObjectsTable.value.d_sortOrder = null;
+  // The page stored after the last DataTablePageEvent must be reset, too.
+  paginationPage.value = 0;
 
-  // reset fields stored between sort and pagination events
-  sortField.value = null;
-  sortOrder.value = null;
-  paginationPage.value = null;
+  // Sorting criteria.
+  eligibleObjectsTable.value.d_sortField = undefined;
+  eligibleObjectsTable.value.d_sortOrder = 0;
 }
-
 </script>
 
 <style scoped></style>
